d9d2846c6af21544baee8fd25a88a4da5f85ed2a
jainullabm <64025654+jainullabm@users.noreply.github.com>
2020-05-11 19:34:51 -0400

    cws

--- src/main/java/com/mj/aws/build/AwsS3Build.java
+++ src/main/java/com/mj/aws/build/AwsS3Build.java
@@ -21,15 +21,12 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.transfer.Upload;
 public class AwsS3Build {
  public static void main(String args[]) {
-	 AWSCredentials credentials = new BasicAWSCredentials(
-			  args[0],args[1]
-			);
-	 
-	 AmazonS3 s3client = AmazonS3ClientBuilder
-			  .standard()
-			  .withCredentials(new AWSStaticCredentialsProvider(credentials))
-			  .withRegion(Regions.US_EAST_2)
-			  .build();
+		/*
+		 * AWSCredentials credentials = new BasicAWSCredentials( args[0],args[1] );
+		 * 
+		 * System.out.println(args[0]); System.out.println(args[1]);
+		 */
+	
 	 
 	// s3client.putObject("www.checkmateinn.com","images/chatboticon.png",new File("C:\\Users\\moham\\Documents\\Innovation\\images\\chatboticon.png"));
 	 
